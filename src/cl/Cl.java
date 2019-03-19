eb42a0bc17815172c107c6e58e9e2c82496ed950
unknown <2098325@labinfo.is.escuelaing.edu.co>
2019-03-19 16:31:13 -0500

    ARRELGADO

--- src/cl/Cl.java
+++ src/cl/Cl.java
@@ -21,16 +21,27 @@ public class Cl {
         Socket s = new Socket("localhost", 4999);
         try {
             while (true) {
-                PrintWriter pr = new PrintWriter(s.getOutputStream());
+                                PrintWriter pr = new PrintWriter(s.getOutputStream());
                 BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-                String linea = stdIn.readLine();
-                pr.println("Esto funciona" + linea);
-                pr.flush();
-                //Parte server
+                
+                                //Parte server
                 InputStreamReader in = new InputStreamReader(s.getInputStream());
                 BufferedReader bf = new BufferedReader(in);
+                System.out.println("llegamossss... " );
                 String str = bf.readLine();
                 System.out.println("Server dijo: " + str);
+                
+                //str sera la sentencia de control que se 
+                //mandara
+                
+                //Aqui se ejecutara el exce, comando cmd en java.
+                //Y capturar respuesta en un string
+                //El string es str
+                
+                //String linea = stdIn.readLine();
+                pr.println("Esto funciona" + str);
+                pr.flush();
+
             }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
